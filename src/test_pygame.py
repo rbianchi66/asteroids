c91b992e7fb51b70edafa3950ad252eade39a545
Renzo Bianchi <rbianchi@comelz.com>
2019-04-29 16:19:06 +0200

    Test with 2 asteroids

--- src/test_pygame.py
+++ src/test_pygame.py
@@ -1,27 +1,28 @@
 import sys, pygame
 import time
+import geo2d
+from asteroid import Asteroid
+from trajectory import Trajectory
 pygame.init()
 
-size = width, height = 320, 240
+size = width, height = 800,600
 speed = [2, 2]
 black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
-
-ball = pygame.image.load("intro_ball.gif")
-ballrect = ball.get_rect()
+objects = [
+            Asteroid(50, Trajectory(geo2d.P(100,10), geo2d.P(1000, 1000))),
+            Asteroid(90, Trajectory(geo2d.P(200,600), geo2d.P(1000, 0))),
+           ]
 
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
-
     screen.fill(black)
-    screen.blit(ball, ballrect)
+    for o in objects:
+        o.draw(screen)
     pygame.display.flip()
-    time.sleep(0.02)
+    time.sleep(0.05)
+    for o in objects:
+        o.updatePos()
